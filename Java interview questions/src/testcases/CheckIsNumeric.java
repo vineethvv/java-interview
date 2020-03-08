05b00f3bd5eacd8010634fca69eacef4e00bb6bb
vineethvv143 <vineeth.g773@gmail.com>
2020-03-08 11:50:31 +0530

    updated

---
+++ Java interview questions/src/testcases/CheckIsNumeric.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class CheckIsNumeric {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
--- Java interview questions/src/testcases/DuplicateWordsinString.java
+++ Java interview questions/src/testcases/DuplicateWordsinString.java
@@ -1,11 +1,64 @@
 package testcases;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class DuplicateWordsinString {
+	
+	// Hey java is java best language is java 
+	//java - 3
+	//is -2
 
 	public static void main(String[] args) {
 
-		
+		findDuplicatewords("Hey java is java best language is java is");
 		
 	}
+	
+	public static void findDuplicatewords(String inputstring) {
+		
+		// split
+		
+		String words[]= inputstring.split(" ");
+		
+		// create one hash map
+		
+		Map<String, Integer> wordcount = new HashMap<String, Integer>();
+		
+		// To check each word in given array ie words
+		
+		for ( String word : words) {
+			
+			
+			// if word is present in the array
+			
+			if(wordcount.containsKey(word)) {
+				
+				wordcount.put(word.toLowerCase(), wordcount.get(word)+1);
+			}
+			
+			else {
+				
+				wordcount.put(word, 1);
+			}
+		}
+	
+	// extracting all the keys of map wordcount
+		
+		Set<String> wordsinstring = wordcount.keySet();
+		
+		// loop through all the words in wordcount
+		
+		for( String word : wordsinstring) {
+			
+			
+			if (wordcount.get(word)>1) {
+				
+				System.out.println(word+" : " + wordcount.get(word));
+			}
+		}
+	}
+	
 
 }
